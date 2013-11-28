67d49dacc5c8ca5f5ed7447df1a6c5f918c26191
Lily <qvang.j@gmail.com>
2013-11-27 16:42:33 -0800

    first attempt to make the card flip

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
-function Matcher($scope){
-  $scope.all = [
-    {
-      "id": "1",
-      "imageUrl": "images/rabbits/rabbit-1"
-    }];
+var mamamatcher = angular.module("mamamatcher", []);
+
+mamamatcher.controller("MainCtrl", ['$scope', function($scope){
+  $scope.shake = function(arr){
+    return _.shuffle(arr)
+  }
 
   $scope.init = function(){
-    var rabbitsNum = 8;
-    for(var i = 1; i <= rabbitsNum; i += 1){
-      $scope.all.push({"id": i, "imageUrl": "images/rabbits/rabbit-" + i})
-    }
-    
+    var rabbitsNum = 7;
+    $scope.cards = [];
+    for(var i = 0; i <= rabbitsNum; i += 1){
+      $scope.cards.push({"id": i, "imageUrl": "images/rabbits/rabbit-" + i});
+      $scope.cards.push({"id": i, "imageUrl": "images/rabbits/rabbit-" + i});
+    };
+    $scope.cards = $scope.shake($scope.cards);
+
   }; 
-}
+}]);
+
+
+$(document).ready(function(){
+  $(".table").hide();
+  $(".start-bn").on('click', function(){
+    $(this).hide();
+    $(".table").show();
+  })
+  $(".card").on('click', function(){
+    $(this).css("-webkit-transform", "rotateY(180deg)")
+    $(this).css("display", "visible");
+  })
+})
